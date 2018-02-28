e05ef37e0c8ed7029067e23ceaf0416cccea7a4c
Alberto <alnavarro@cloud.hosts.mx>
2018-02-28 03:16:08 +0000

    Adding color

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 let Parser = new window.RSSParser();
 const CORS_PROXY = "/rss/"; 
-//"https://cors-anywhere.herokuapp.com/"
 const url = "www.npr.org/rss/podcast.php?id=510289";
 
 class App extends Component {
