14f8712dca5305e5746fd1367cd9889aa02a6962
Alberto Navarro <navio@users.noreply.github.com>
2018-05-13 22:19:18 -0400

    Updating Player to listen to audio.

--- src/index.js
+++ src/index.js
@@ -7,25 +7,34 @@ const url = "www.npr.org/rss/podcast.php?id=510289";
 class App extends Component {
   constructor() {
     super();
-    
     this.state = {
       name: 'React',
-      items: null
+      items: null,
+      src:null
     };
+    this.playerHandler = this.playerHandler.bind(this);
+  }
+  playerHandler(ev){
+    let src = ev.target.getAttribute('data-src');
+    this.setState({src});
   }
   componentDidMount(){
     Parser.parseURL(CORS_PROXY+url)
     .then(x=>this.setState({items:x.items}));
   }
-
+  toMinutes(time){
+    return Math.floor( 1* time / 60 ) + ':'+ (1*time%60);
+  }
   render() {
-    console.log('a',this.state.items)
     return (
       <div>
+        <audio autoPlay="true" src={this.state.src} ref="player"/>
         <ol>
          {this.state.items && 
           this.state.items.map(item=><li><a href="{item.link}">
-          {item.title}</a></li>)}
+          {item.title}</a> ({this.toMinutes(item.itunes.duration)}) <a onClick={this.playerHandler} data-src={item.enclosure.url}>PLAY</a><br />{item.content}
+          
+          </li>)}
         </ol>
       </div>
     );
