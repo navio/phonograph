bcdca8df159f6fa7b661ffa18b1e0d2f2651f4c8
Alberto Navarro <alberton@penguin.test>
2020-03-01 21:40:50 -0500

    Fixes to search

--- src/engine/podcast/PodcastSearcher.js
+++ src/engine/podcast/PodcastSearcher.js
@@ -17,11 +17,19 @@ export default class PodcastSearcher {
     this.currentRequest && this.currentRequest.abort();
     this.currentRequest = new AbortController();
     let { signal } = this.currentRequest;
-    const api = `https://listen-api.listennotes.com/api/v2/search?q=${term}&sort_by_date=0&type=po&offset=0&len_min=10&len_max=30&only_in=title&safe_mode=1`;
+    const api = `https://listen-api.listennotes.com/api/v2/search?q=${term}&sort_by_date=0&type=podcasts&offset=0&len_min=10&len_max=30&only_in=title&safe_mode=1`;
     const headers = {
-      'X-ListenAPI-Key': '5b7650d15f5b4edea86257fe42791e22'
+      'X-ListenAPI-Key': '5b7650d15f5b4edea86257fe42791e22',
+      'User-Agent': 'podcastsuite',
+      'Accept': 'application/rss+xml'
     }
     const response = fetch(api, { headers, signal }).then(results => results.json());
     return response;
   };
+
+  static getFinalURL(url) {
+    const method = "HEAD";
+    fetch(domain, {method})
+    .then(response => response.he)
+  }
 }
