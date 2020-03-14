ae1f438b8209fb0a63dde9d8e5dc67e7c70d5929
Alberto Navarro <alberton@penguin.test>
2020-03-14 14:33:09 -0400

    Changin size to correct iphone issues

--- src/podcast/PodcastGrid.js
+++ src/podcast/PodcastGrid.js
@@ -29,7 +29,7 @@ export const styles = theme => ({
       ,width:0
   },
   relativeContainer:{
-    position:"relative"
+    position:"relative",
   },
   addIcon:{
     width:'3em',
@@ -74,7 +74,7 @@ function PodCastGrid(props) {
       return (
       <Grid container spacing={0} direction={'row'}>
         { casts && casts.map(cast =>
-          <Grid item xs={3} sm={2} md={1} key={cast.domain} >
+          <Grid item xs={4} sm={3} md={2} key={cast.domain} >
             <Card classes={{root:classes.card}} style={getMyColor(cast)}>
               { cast.domain === addMore
               ? <IconButton onClick={cast.onClick} classes={{root:classes.card}}>
