719b3b1a39608376fbbd32de7f50c5ff89248e02
alberto ☁ <alnavarro@gmail.com>
2020-03-26 21:30:20 +0000

    Size and Service Worker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+
+render(<Router><App /></Router>, document.getElementById('root'));
+
+serviceWorker.unregister();
