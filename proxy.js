385ac5951042d0888fdbea4c12460851e743a53d
alberto ☁ <alnavarro@gmail.com>
2020-04-06 00:57:07 +0000

    State for private SW

--- proxy.js
+++ proxy.js
@@ -1,5 +1,10 @@
 const Bundler = require('parcel-bundler');
 const express = require('express');
+
+const fs = require('fs');
+const https = require('https');
+const http = require('http');
+
 const {createProxyMiddleware: proxy} = require('http-proxy-middleware');
 
 const app = express();
@@ -19,4 +24,22 @@ app.use('/ln', proxy({
 const bundler = new Bundler('public/index.html');
 app.use(bundler.middleware());
 
-app.listen(Number(process.env.PORT || 1234));
+// app.listen(Number(process.env.PORT || 1234));
+
+
+const cert = "/etc/letsencrypt/live/cloud.hosts.mx/fullchain.pem";
+const key = "/etc/letsencrypt/live/cloud.hosts.mx/privkey.pem";
+
+const privateKey  = fs.readFileSync(key);
+const certificate = fs.readFileSync(cert);
+const credentials = {key: privateKey, cert: certificate};
+
+https.createServer(credentials, app)
+.listen(1234, () => console.log("Running!"));
+
+http.createServer(function (req, res) {
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    res.end();
+  }).listen(4234);
+
+  
