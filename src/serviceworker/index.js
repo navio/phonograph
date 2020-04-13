4aa1a776735a1b97d1baf52eb850b3f9e104290f
alberto ☁ <alnavarro@gmail.com>
2020-04-13 03:15:41 +0000

    Updatinb SW

---
+++ src/serviceworker/index.js
@@ -0,0 +1,11 @@
+export default () => {
+  if ('serviceWorker' in navigator) {
+    // Use the window load event to keep the page load performant
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register("./service-worker.js")
+        .then(() => console.log("SW Registered"))
+        .catch(() => console.error("failed to register service worker!"));
+    });
+  }
+}
