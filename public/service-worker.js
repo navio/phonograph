f8499116b233fb55e138685fa090eeb8a243f3b4
alberto ☁ <alnavarro@gmail.com>
2020-05-15 15:59:32 +0000

    Updates Fixes

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,53 +1,8 @@
 const version = 1.8;
 const CACHE = 'phonograph-core-' + version;
+const CACHERUNTIME = 'phonograph-runtime-' + version;
 
 
-const ignoreMe = (url) => {
-
-  if(url.indexOf('itunes.apple.com')){
-    return true;
-  }
-
-  if (url.indexOf('chrome-extension') > -1) {
-    return true;
-  }
-  
-  if (url.indexOf('/api/') > -1) {
-    return true;
-  }
-  
-  if (url.indexOf('/rss-full/') > -1) {
-    return true;
-  }
-  
-  if (url.indexOf('/ln/') > -1) {
-    return true;
-  }
-
-  if (url.indexOf('wss://') > -1) {
-    return true;
-  }
-
-  if (url.indexOf('/ignoreme/') > -1) {
-    return true;
-  }
-
-  return false;
-} 
-
-const shouldUpdate = (url) => {
-  if (url.indexOf('.png') > -1) {
-    return false;
-  }
-  if (url.indexOf('.jpg') > -1) {
-    return false;
-  }
-  if (url.indexOf('.mp3') > -1) {
-    return false;
-  }
-
-  return true;
-}
 
 self.addEventListener('install', function (event){
   event.waitUntil(
@@ -75,86 +30,135 @@ self.addEventListener('activate', event => {
 
 
 self.addEventListener("fetch", function (evt) {
-  evt.respondWith(getOrGetAndStore(evt.request));
+  if (evt.request.method === 'GET' && !ignoreMe(evt.request.url) ) {
+    fetchTask(evt);
+  }
 });
 
-function getOrGetAndStore(request) {
+function fetchTask(event) {
 
-  let other;
+    const {request} = event;
 
-  if (request.method !== 'GET') {
-    return fetch(request);
+    event.respondWith(
+      caches.match(request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return caches.open(CACHERUNTIME)
+              .then(function(cache) { 
+                console.log('fetching',request);
+                return fetch(request).then(response => {
+                  if (!response.ok) {
+                    return response;
+                  }
+                  return cache.put(request, response.clone())
+                  .then(() => {
+                    return response;
+                  });
+                });
+              });
+      })
+    )
+
   }
 
-  if (ignoreMe(request.url)) {
-    return fetch(request);
-  }
 
-  if (request.url.indexOf('/library') > -1) {
-    const ref = request.referrer + 'library';
-    const final = request.url.slice(ref)[0];
-    if (final) {
-      other = request.referrer;
+  const ignoreMe = (url) => {
+    if(url.indexOf('itunes.apple.com') > -1 ){
+      return true;
     }
-  }
-
-  if (request.url.indexOf('/podcast') > -1) {
-    const ref = request.referrer + 'podcast';
-    const final = request.url.slice(ref)[0];
-    if (final) {
-      other = request.referrer;
+  
+    if (url.indexOf('chrome-extension') > -1) {
+      return true;
     }
-  }
-
-  if (request.url.indexOf('/discover') > -1) {
-    const ref = request.referrer + 'discover';
-    const final = request.url.slice(ref)[0];
-    if (final) {
-      other = request.referrer;
+    
+    if (url.indexOf('/api/') > -1) {
+      return true;
     }
-  }
-
-  if (request.url.indexOf('/settings') > -1) {
-    const ref = request.referrer + 'settings';
-    const final = request.url.slice(ref)[0];
-    if (final) {
-      other = request.referrer;
+    
+    if (url.indexOf('/rss-full/') > -1) {
+      return true;
     }
-  }
+    
+    if (url.indexOf('/ln/') > -1) {
+      return true;
+    }
+  
+    if (url.indexOf('wss://') > -1) {
+      return true;
+    }
+  
+    if (url.indexOf('/ignoreme/') > -1) {
+      return true;
+    }
+  
+    return false;
+  } 
+  
+  // const shouldUpdate = (url) => {
+  //   if (url.indexOf('.png') > -1) {
+  //     return false;
+  //   }
+  //   if (url.indexOf('.jpg') > -1) {
+  //     return false;
+  //   }
+  //   if (url.indexOf('.mp3') > -1) {
+  //     return false;
+  //   }
+  
+  //   return true;
+  // }
 
- return caches.open(CACHE)
-  .then(function(cache) {
-    return cache.match(other || request)
-    .then(function(res) {
-      if(res){
-        return res;
-      }else{
-        return fetch(other || request)
-        .then(function(response) {
-          if (!response.ok) {
-            return response;
-          }
-          cache.put((other || request), response.clone());
-          return response;
-        })
-      }
-    })
-  });
-}
 
-function update(request) {
-  if (ignoreMe(request.url)){
-    return;
-  }
-  if(!shouldUpdate(request.url)){
-    return;
-  }
+  // const localURLs = ['library', 'podcast', 'discover', 'settings' ]
+  // .map(path => event.request.referrer + path );
 
-  return caches.open(CACHE).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response.clone()).then(function () {
-        return response;
-      });
-    });
-  });
-}
+  // if (request.url.indexOf('/library') > -1) {
+  //   const ref = request.referrer + 'library';
+  //   const final = request.url.slice(ref)[0];
+  //   if (final) {
+  //     other = request.referrer;
+  //   }
+  // }
+
+  // if (request.url.indexOf('/podcast') > -1) {
+  //   const ref = request.referrer + 'podcast';
+  //   const final = request.url.slice(ref)[0];
+  //   if (final) {
+  //     other = request.referrer;
+  //   }
+  // }
+
+  // if (request.url.indexOf('/discover') > -1) {
+  //   const ref = request.referrer + 'discover';
+  //   const final = request.url.slice(ref)[0];
+  //   if (final) {
+  //     other = request.referrer;
+  //   }
+  // }
+
+  // if (request.url.indexOf('/settings') > -1) {
+  //   const ref = request.referrer + 'settings';
+  //   const final = request.url.slice(ref)[0];
+  //   if (final) {
+  //     other = request.referrer;
+  //   }
+  // }
+
+
+// function update(request) {
+//   if (ignoreMe(request.url)){
+//     return;
+//   }
+//   if(!shouldUpdate(request.url)){
+//     return;
+//   }
+
+//   return caches.open(CACHE).then(function (cache) {
+//     return fetch(request).then(function (response) {
+//       return cache.put(request, response.clone()).then(function () {
+//         return response;
+//       });
+//     });
+//   });
+// }
