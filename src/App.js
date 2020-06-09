2cfb989828733c370bef7e5a5df61ed000b2fc19
alberto ☁ <alnavarro@gmail.com>
2020-06-09 03:51:23 +0000

    Updating

--- src/App.js
+++ src/App.js
@@ -213,6 +213,7 @@ const App = ({}) => {
         </Suspense>
 
         <audio
+          preload="metadata"
           autoPlay={state.status !== "pause"}
           ref={player}
           preload="auto"
