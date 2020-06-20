8edce76aa338516df2c26e024ff746d9cc19773d
alberto ☁ <alnavarro@gmail.com>
2020-06-20 15:12:46 +0000

    Env Variables

--- filegetter.sh
+++ filegetter.sh
@@ -1,9 +1,11 @@
-set APITOKEN = listennotes
+# set APITOKEN = listennotes
+echo $LISTENNOTES;
+echo "X-ListenAPI-Key: $LISTENNOTES";
 curl 'https://listen-api.listennotes.com/api/v2/best_podcasts?region=us&safe_mode=0' \
-  -H 'X-ListenAPI-Key: $listennotes' > ./src/podcast/Discovery/top.json
+  -H "X-ListenAPI-Key: $LISTENNOTES" > ./src/podcast/Discovery/top.json
 
 curl 'https://listen-api.listennotes.com/api/v2/genres?top_level_only=1' \
-  -H 'X-ListenAPI-Key: $listennotes' > ./src/podcast/Discovery/genres.json
+  -H "X-ListenAPI-Key: $LISTENNOTES" > ./src/podcast/Discovery/genres.json
 
 curl 'https://listen-api.listennotes.com/api/v2/curated_podcasts?page=2' \
-  -H 'X-ListenAPI-Key: $listennotes' > ./src/podcast/Discovery/curated.json
+  -H "X-ListenAPI-Key: $LISTENNOTES" > ./src/podcast/Discovery/curated.json
