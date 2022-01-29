79004ffb26a2c0b4ee72204d330ab1fe7a7ae7eb
alberto ☁ <alnavarro@gmail.com>
2022-01-29 20:02:46 +0000

    Updating Search Enpoint

--- src/podcast/Discovery/PodcastSearcher.js
+++ src/podcast/Discovery/PodcastSearcher.js
@@ -41,7 +41,7 @@ export default class PodcastSearcher {
   }
 
   apple(term) {
-    return this.querySearch("`https://itunes.apple.com/search?media=podcast&limit=20&term=${term}`", term);
+    return this.querySearch(`https://podcast-search.navio.workers.dev/term=${term}`, term);
   }
 
 
